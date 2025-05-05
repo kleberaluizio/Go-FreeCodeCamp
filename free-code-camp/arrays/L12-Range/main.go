d0b9a27088b2490fdcbbf43ec65e88f7dd054e0c
kleberaluizio <kleberaluizio@gmail.com>
2025-05-05 17:23:15 -0300

    Entendendo o uso de arrays e slices - parte 2

--- free-code-camp/arrays/L12-Range/main.go
+++ free-code-camp/arrays/L12-Range/main.go
@@ -2,9 +2,9 @@ package main
 
 func indexOfFirstBadWord(msg []string, badWords []string) int {
 
-	for i, m := range msg {
+	for i, word := range msg {
 		for _, badWord := range badWords {
-			if m == badWord {
+			if word == badWord {
 				return i
 			}
 		}
